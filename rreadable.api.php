49490d23eabb47ff8e8d0322d5debaabe947b85f
Tim Whitney <tim.d.whitney@gmail.com>
2016-06-23 10:11:51 -0600

    Initial Add

---
+++ rreadable.api.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ * File for
+ */
+
+/**
+ * Alter the desc before it is passed into the output function.
+ */
+function hook_rreadable_desc_alter(&$desc) {
+
+}
+
+/**
+ * Alter the rrule parts before they go into the parsing mechanism.
+ */
+function hook_rreadable_rrule_parts_alter(&$parts) {
+
+}
